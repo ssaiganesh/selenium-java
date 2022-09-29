f15651b939ec08ee2d840e80f4d9cf532803fb0a
Shankar Sai Ganesh <shankarsaiganesh@gmail.com>
2022-09-29 11:46:14 +0800

    practice with dropdown

--- src/com/selenium/seleniumDropDown.java
+++ src/com/selenium/seleniumDropDown.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class seleniumDropDown {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         // 1. Open Chrome Browser
         System.setProperty("webdriver.chrome.driver", "D://selenium jar and drivers//chromedriver//chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -24,6 +24,38 @@ public class seleniumDropDown {
         Select se=new Select(element);
         se.selectByIndex(1);
 
+        Thread.sleep(3000);
+
+        driver.get("C://Users//shank//IdeaProjects//selenium//templates//dropdown-value-select.html");
+
+        WebElement valueElement=driver.findElement(By.name("Mobiles"));
+        Select valueSe = new Select(valueElement);
+        valueSe.selectByValue("nokia");
+
+
+        Thread.sleep(3000);
+
+//        se.selectByVisibleText("HTC");
+//
+//        Thread.sleep(2000);
+
+        driver.get("C://Users//shank//IdeaProjects//selenium//templates//dropdown-multiselect.html");
+
+        WebElement multiSelectElement=driver.findElement(By.name("Mobdevices"));
+        Select multiSe=new Select(multiSelectElement);
+        /* OR
+         Select multiSe=new Select(driver.findElement(By.name("Mobdevices")));
+        **/
+
+//        se.selectByVisibleText("HTC");
+        multiSe.selectByIndex(1);
+
+        Thread.sleep(3000);
+
+//        se.deselectByValue("nokia");
+//        se.deselectByIndex(1); // if this specific index is selected
+//
+//        se.deselectByVisibleText("HTC");
 
 
     }
